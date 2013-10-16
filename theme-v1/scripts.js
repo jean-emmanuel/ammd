050390837ca2a348b5b330a13b655f3b599598d0
jean-emmanuel <doucet.duke@gmail.com>
2013-10-16 13:59:08 +0200

    a

--- theme-v1/scripts.js
+++ theme-v1/scripts.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
 	$('.minmax').click(function(){
 		
 		if (!$('.wrapper-content').hasClass('mini')) {
-			$('.minmax i').removeClass('icon-eye-open').addClass('icon-eye-close');
+			$('.minmax i').removeClass('icon-collapse-top').addClass('icon-collapse');
 			if (!$('.minmax').hasClass('clicked')) {h = $('.wrapper-content').height();$('.minmax').addClass('clicked');}
 			$('.wrapper-content').addClass('mini').animate({height:'24px'});
 		} else { 
 			$('.wrapper-content.mini').removeClass('mini').animate({height:h});
-			$('.minmax i').addClass('icon-eye-open').removeClass('icon-eye-close');
+			$('.minmax i').addClass('icon-collapse-top').removeClass('icon-collapse');
 		}
 		return false
 	});
