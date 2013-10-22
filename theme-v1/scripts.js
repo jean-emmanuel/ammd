af6dce9e89905a0ce668b57b3aa6e18668c69df6
jean-emmanuel <doucet.duke@gmail.com>
2013-10-22 16:36:38 +0200

    mobile++

--- theme-v1/scripts.js
+++ theme-v1/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$('.blockquote > :last-child').addClass('last-child')
 	$('.nav a').focus(function(){$(this).parent('li').addClass('focus')});
 	$('.nav a').blur(function(){$(this).parent('li').removeClass('focus')});
+	if ($('#navigation').height()>30) {$('.nav').addClass('mobile')}
 	$('.minmax').click(function(){
 		
 		if (!$('.wrapper-content').hasClass('mini')) {
@@ -14,4 +15,4 @@ $(document).ready(function(){
 		}
 		return false
 	});
-})	
+})
