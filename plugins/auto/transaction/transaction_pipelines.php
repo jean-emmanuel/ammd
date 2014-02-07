50acbec1bd862ebbadf66cc5115a78eda9314a8c
jean-emmanuel <doucet.duke@gmail.com>
2014-02-07 12:06:27 +0100

    v3

--- plugins/auto/transaction/transaction_pipelines.php
+++
@@ -1,6 +0,0 @@
-<?php
-      function transaction_insert_head($texte){
-          $texte .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/transaction.css').'" media="all" />'."\n";
-          return $texte;
-      }
-?>
