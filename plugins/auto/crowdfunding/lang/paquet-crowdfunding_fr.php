483faede0ebc10f412d0718068c57037462fb0fb
jean-emmanuel <doucet.duke@gmail.com>
2014-05-06 12:35:49 +0200

    financement

--- plugins/auto/crowdfunding/lang/paquet-crowdfunding_fr.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!defined('_ECRIRE_INC_VERSION')) return;
- 
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-'crowdfunding_nom' => "Crowdfunding",
-'crowdfunding_slogan' => "Financement participatif avec Paypal",
-);
-?>
