f78ac82a1178b1fa68e5d13835ee320406274e77
jean-emmanuel <doucet.duke@gmail.com>
2014-05-06 12:46:06 +0200

    o

---
+++ plugins/auto/financement/financement_autorisations.php
@@ -0,0 +1,9 @@
+<?php
+  if (!defined('_ECRIRE_INC_VERSION')) return;
+  function autoriser_financement_transaction_voir_dist($faire, $type, $id, $qui, $opt) {
+    return autoriser('modifier', 'financement_transaction', $id, $qui, $opt);
+  }
+  function autoriser_financement_transaction_modifier_dist($faire, $type, $id, $qui, $opt) {
+    return in_array($qui['statut'], array('0minirezo', '1comite'));
+  }
+?>
---
+++ plugins/auto/financement/lang/financement_fr.php
@@ -0,0 +1,52 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// I
+	'icone_creer_financement' => "Créer un financement",
+	'icone_modifier_financement' => "Modifier ce financement",
+	
+	'icone_creer_financement_transaction' => "Ajouter une transaction",
+	'icone_modifier_financement_transaction' => "Modifier cette transaction",
+	
+	'info_aucun_financement' => "Aucun financement",
+	
+	'info_1_financement' => "Un financement",
+	'info_nb_financements' => "@nb@ financements",	
+	
+	'info_1_transaction' => "Une transaction",
+	'info_nb_transactions' => "@nb@ transactions",
+
+	//L
+	'label_titre' => " Titre",
+	'label_date_debut' => " Date de début",
+	'label_date_echeance' => " Date d'échéance",
+	'label_montant_demande' => " Montant demandé (€)",
+	'label_email' => " Email Paypal à créditer",
+
+	'label_transaction' => "Transaction",
+	'label_transaction_nom' => "Nom",
+	'label_transaction_prenom' => "Prénom",
+	'label_transaction_date' => "Date",
+	'label_transaction_montant' => "Montant",
+	'label_transaction_email' => "Email",
+	'label_transaction_adresse' => "Adresse de livraison",
+	 
+	// T
+	'titre_financement' => "Financement",
+	'titre_financements' => "Financements",
+  'titre_logo_financement' => "Logo de ce financement",
+  'titre_financements___transactions' => "Transactions",
+  'titre_financement_transaction' => "Transaction",
+  'titre_logo_financements_transactions' => "Logo de cette transaction",
+
+	// P
+	'paypal_message' => "Envoyer un message",
+	'paypal_submit'  => "Soutenir ce projet",
+	
+	// S
+	'supprimer_financement' => "Supprimer ce financement",
+	'supprimer_financement_transaction' => "Supprimer cette transaction",
+	
+);
+    
+?>
